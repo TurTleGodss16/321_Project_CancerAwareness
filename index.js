c913172e4b0da123ffb0834761e63ed156aed6f4
TurtleGodss16 <dat130101@gmail.com>
2023-09-23 02:15:03 +1000

    New Update

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './React/App';
 import {name as appName} from './app.json';
+import Navigation from './React/Navigation';
 
 AppRegistry.registerComponent(appName, () => App);
