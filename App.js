481241f6df66b1839b8fa27d6a53f7f867561413
TurtleGodss16 <dat130101@gmail.com>
2023-09-23 01:13:25 +1000

    Update

---
+++ App.js
@@ -0,0 +1,21 @@
+
+const App = () => {
+  /*const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <View style={styles.container}>
+      
+      <View style={[styles.menuBar, isMenuOpen ? styles.openMenuBar : {}]}>
+        <TouchableOpacity onPress={toggleMenu} style={styles.menuToggle}>
+          <Text style={styles.menuToggleText}>☰</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );*/
+};
+
+export default App;
