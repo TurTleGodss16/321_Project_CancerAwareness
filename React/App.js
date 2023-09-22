bc93ec3442d31420bcff62bfca76a9b6cedbc4c9
TurtleGodss16 <dat130101@gmail.com>
2023-09-23 01:39:14 +1000

    New Update

---
+++ React/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import styles from './Styles'; // Import styles from the CSS file
+
+const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Menu Bar */}
+      <View style={[styles.menuBar, isMenuOpen ? styles.openMenuBar : {}]}>
+        <TouchableOpacity onPress={toggleMenu} style={styles.menuToggle}>
+          <Text style={styles.menuToggleText}>☰</Text>
+        </TouchableOpacity>
+        <View style={styles.menuSections}>
+          <TouchableOpacity style={styles.menuSection}>
+            <Text>Account</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuSection}>
+            <Text>Bookmarks</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuSection}>
+            <Text>Info</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* Main Content */}
+      <View style={styles.mainContent}>
+        <Text style={styles.heading}>Welcome to Our Company</Text>
+        <Text>We provide innovative solutions for your needs.</Text>
+      </View>
+    </View>
+  );
+};
+
+export default App;
---
+++ React/Styles.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  menuBar: {
+    width: 60,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+  },
+
+  openMenuBar: {
+    width: 200,
+  },
+
+  menuToggle: {
+    marginBottom: 20,
+  },
+
+  menuToggleText: {
+    color: 'white',
+    fontSize: 24,
+  },
+
+  menuSections: {
+    flex: 1,
+  },
+
+  menuSection: {
+    padding: 10,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+  },
+
+  mainContent: {
+    flex: 1,
+    paading: 20,
+  },
+
+  heading: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+});
