c1fae8cf27251d6841daf6b801c3c9ed35d23815
TurtleGodss16 <dat130101@gmail.com>
2023-09-23 19:13:44 +1000

    New Update

---
+++ React/AccountScreen.js
@@ -0,0 +1,14 @@
+//Account Screen
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AccountScreen = () => {
+  return (
+    <View>
+      <Text>Account Page</Text>
+    </View>
+  );
+};
+
+export default AccountScreen;
