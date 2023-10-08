385887b289c78230e5f56ece1ab10c103a00f301
Mobeen Gaming PC <mobeenk89@gmail.com>
2023-10-08 20:56:07 +1100

    Library Text udpate

--- React/BookmarkScreen.js
+++ React/BookmarkScreen.js
@@ -3,6 +3,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const BookmarkScreen = ({navigation}) => {};
+const BookmarkScreen = ({navigation}) => {
+    
+    return (
+        <View>
+          <Text>Library</Text>
+        </View>
+    );
+};
+
 
 export default BookmarkScreen;
