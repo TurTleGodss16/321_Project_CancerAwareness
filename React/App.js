0da81425f3b34a2d9e79303301c8ddd9b80075f2
TurtleGodss16 <dat130101@gmail.com>
2023-10-10 00:39:13 +1100

    New Update On Navigation

--- React/App.js
+++ React/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState, useRef} from 'react';
 import {Text, TouchableOpacity, View, Animated, Easing} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './MainScreen';
 import AccountScreen from './AccountScreen';
@@ -11,7 +11,8 @@ import BookmarkScreen from './BookmarkScreen';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
+const Menu = () => {
+  const navigation = useNavigation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuAnimation = useRef(new Animated.Value(0)).current;
 
@@ -40,53 +41,75 @@ const App = () => {
   };
 
   return (
-    <NavigationContainer>
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        {/* Menu */}
-        <Animated.View
-          style={[
-            {backgroundColor: '#ecf0f1', padding: 10, paddingTop: 10},
-            menuStyle,
-          ]}>
-          <TouchableOpacity onPress={() => console.log('Account')}>
-            <Text>Account</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => console.log('About')}>
-            <Text>About Us</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => console.log('Setting')}>
-            <Text>Setting</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => console.log('Bookmark')}>
-            <Text>Bookmark</Text>
-          </TouchableOpacity>
-        </Animated.View>
-
-        {/* Main Content */}
-        <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen
-            name="Main"
-            component={MainScreen}
-            options={{title: 'Cancer Awareness'}}
-          />
-          <Stack.Screen name="Account" component={AccountScreen} />
-          <Stack.Screen name="About" component={AboutScreen} />
-          <Stack.Screen name="Setting" component={SettingScreen} />
-          <Stack.Screen name="Bookmark" component={BookmarkScreen} />
-        </Stack.Navigator>
-
-        {/* Menu Toggle Button */}
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      {/* Menu */}
+      <Animated.View
+        style={[
+          {backgroundColor: '#ecf0f1', padding: 10, paddingTop: 10},
+          menuStyle,
+        ]}>
         <TouchableOpacity
-          onPress={toggleMenu}
-          style={{
-            position: 'absolute',
-            top: 20,
-            left: 20,
-            zIndex: 2,
+          onPress={() => {
+            toggleMenu();
+            navigation.navigate('Account');
           }}>
-          <Text style={{color: 'black', fontSize: 20}}>☰</Text>
+          <Text>Account</Text>
         </TouchableOpacity>
-      </View>
+        <TouchableOpacity
+          onPress={() => {
+            toggleMenu();
+            navigation.navigate('About');
+          }}>
+          <Text>About Us</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            toggleMenu();
+            navigation.navigate('Setting');
+          }}>
+          <Text>Setting</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            toggleMenu();
+            navigation.navigate('Bookmark');
+          }}>
+          <Text>Bookmark</Text>
+        </TouchableOpacity>
+      </Animated.View>
+
+      {/* Main Content */}
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{title: 'Cancer Awareness'}}
+        />
+        <Stack.Screen name="Account" component={AccountScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Setting" component={SettingScreen} />
+        <Stack.Screen name="Bookmark" component={BookmarkScreen} />
+      </Stack.Navigator>
+
+      {/* Menu Toggle Button */}
+      <TouchableOpacity
+        onPress={toggleMenu}
+        style={{
+          position: 'absolute',
+          top: 20,
+          left: 20,
+          zIndex: 2,
+        }}>
+        <Text style={{color: 'black', fontSize: 20}}>☰</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Menu />
     </NavigationContainer>
   );
 };
