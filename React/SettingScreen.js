b83b57c112ed4c9dd1f4559427797fd6a2718275
TurtleGodss16 <dat130101@gmail.com>
2023-10-10 13:14:30 +1100

    New Update on Settings Screen

--- React/SettingScreen.js
+++ React/SettingScreen.js
@@ -1,8 +1,114 @@
 //Setting Screen
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const SettingScreen = ({navigation}) => {};
+const SettingScreen = ({navigation}) => {
+  return (
+    <View style={styles.body}>
+      <View style={styles.containers}>
+        <View>
+          <Image
+            style={styles.image_custom_notification}
+            source={require('../Images/Notification_Bell.png')}
+          />
+        </View>
+        <View>
+          <Text style={styles.headerText}>Notifications</Text>
+          <Text style={styles.descriptionText}>
+            Manage notification by type.
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.containers}>
+        <View>
+          <Image
+            style={styles.image_custom_theme}
+            source={require('../Images/Theme.png')}
+          />
+        </View>
+        <View>
+          <Text style={styles.headerText}>Theme</Text>
+          <Text style={styles.descriptionText}>
+            Toggle Light and Dark mode.
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.containers}>
+        <View>
+          <Image
+            style={styles.image_custom_language}
+            source={require('../Images/Language.png')}
+          />
+        </View>
+        <View>
+          <Text style={styles.headerText}>Language</Text>
+          <Text style={styles.descriptionText}>Set the default language.</Text>
+        </View>
+      </View>
+
+      <View style={styles.containers}>
+        <View>
+          <Image
+            style={styles.image_custom_access}
+            source={require('../Images/Accessibility.png')}
+          />
+        </View>
+        <View>
+          <Text style={styles.headerText}>Accessibility Options</Text>
+          <Text style={styles.descriptionText}>
+            Customize your experience for greater accessibility
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'white',
+    fontFamily: 'sans-serif',
+    gap: 10,
+  },
+
+  headerText: {
+    fontWeight: 'bold',
+  },
+
+  descriptionText: {
+    fontSize: 10,
+  },
+
+  containers: {
+    flexDirection: 'row',
+  },
+
+  image_custom_notification: {
+    width: 27,
+    height: 27,
+    top: 3,
+  },
+
+  image_custom_theme: {
+    width: 26,
+    height: 25,
+    top: 5,
+  },
+
+  image_custom_language: {
+    width: 26,
+    height: 25,
+    top: 5,
+  },
+
+  image_custom_access: {
+    width: 26,
+    height: 25,
+    top: 5,
+  },
+});
 
 export default SettingScreen;
