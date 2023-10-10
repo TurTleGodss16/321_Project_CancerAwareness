5aa8f62d5ec2f5456e683f2e4f7e83f8e35aba13
TurtleGodss16 <dat130101@gmail.com>
2023-10-10 16:24:59 +1100

    New Update on Bookmark Screen

--- React/BookmarkScreen.js
+++ React/BookmarkScreen.js
@@ -1,8 +1,57 @@
 //Bookmark Screen
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const BookmarkScreen = ({navigation}) => {};
+const BookmarkScreen = ({navigation}) => {
+  return (
+    <View style={styles.body}>
+      <View>
+        <Image
+          style={styles.image_custom}
+          source={require('../Images/Bookmark_Warning.png')}
+        />
+      </View>
 
+      <View style={styles.text_custom}>
+        <Text style={styles.headerText}>You have no bookmarks!</Text>
+        <Text style={styles.descriptionText}>
+          Click the flag icon on any article/page and it will appear here!
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'white',
+    gap: 10,
+  },
+
+  image_custom: {
+    width: 100,
+    height: 100,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+
+  text_custom: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+
+  headerText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  descriptionText: {
+    fontSize: 10,
+    textAlign: 'center',
+  },
+});
 export default BookmarkScreen;
