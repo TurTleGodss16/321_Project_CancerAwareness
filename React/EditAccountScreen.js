978c4d2d523c65269d71de8db90f5ed06493b8e2
Mobeen Gaming PC <mobeenk89@gmail.com>
2023-10-14 13:38:07 +1100

    updated EditAcountScreen and imagePicker import

--- React/EditAccountScreen.js
+++ React/EditAccountScreen.js
@@ -1,15 +1,25 @@
 //Edit account
-/*
+
 import React from 'react';
 import {View, Text, Image, StyleSheet, TextInput, Button, TouchableOpacity, ScrollView} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import * as ImagePicker from "expo-image-picker";
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const EditProfileScreen = ({navigation, route}) => {
 
-    const [selectedImage, setSelectedImage] = useState(imagesURL[0])
+    const [selectedImage, setSelectedImage] = useState(imagesURL[0]);
+    const [email, setEmail] = useState("johndoe@gmail.com");
+    const [password, setPassword] = useState("randompassword");
 
-    const handleImageSelection = () => {
+
+    const handleImageSelection = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
 
     }
 
@@ -42,6 +52,83 @@ const EditProfileScreen = ({navigation, route}) => {
                        }}></View>
                     </TouchableOpacity>
                 </View>
+                <View  style = {{
+                    flexDirection: "Column",
+                    marginBottom: 6
+                }}>
+                    <Text style = {{fontSize: 30, fontWeight: "bold"}}>Name</Text>
+                    <View style = {{
+                        height: 44,
+                        width: "100%",
+                        borderColor: "Grey",
+                        borderWidth: 1,
+                        borderRadius: 4,
+                        marginVertical: 6,
+                        justifyContent: "center",
+                        paddingLeft: 8
+                    }}>
+                        <TextInput value = {name} 
+                            onChangeText = {value => setName(value)}
+                            editable = {true}
+                        />
+                    </View>
+                </View>
+                <View  style = {{
+                    flexDirection: "Column",
+                    marginBottom: 6
+                }}>
+                    <Text style = {{fontSize: 30, fontWeight: "bold"}}>Email</Text>
+                    <View style = {{
+                        height: 44,
+                        width: "100%",
+                        borderColor: "Grey",
+                        borderWidth: 1,
+                        borderRadius: 4,
+                        marginVertical: 6,
+                        justifyContent: "center",
+                        paddingLeft: 8
+                    }}>
+                        <TextInput value = {email} 
+                            onChangeText = {value => setEmail(value)}
+                            editable = {true}
+                        />
+                    </View>
+                </View>
+                <View  style = {{
+                    flexDirection: "Column",
+                    marginBottom: 6
+                }}>
+                    <Text style = {{fontSize: 30, fontWeight: "bold"}}>Password</Text>
+                    <View style = {{
+                        height: 44,
+                        width: "100%",
+                        borderColor: "Grey",
+                        borderWidth: 1,
+                        borderRadius: 4,
+                        marginVertical: 6,
+                        justifyContent: "center",
+                        paddingLeft: 8
+                    }}>
+                        <TextInput value = {password} 
+                            onChangeText = {value => 
+                            setName(password)}
+                            secureTextEntry
+                            editable = {true} 
+                        />
+                    </View>
+                </View>
+                <TouchableOpacity style = {{
+                    backgroundColor: "white",
+                    height: 44,
+                    borderRadius: 6,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                    <Text style = {{
+                        fontSize: 16,
+                        fontWeight: "bold",
+                    }}>Save</Text>
+                </TouchableOpacity>
             </ScrollView>
         </SafeAreaView>
 
@@ -49,4 +136,3 @@ const EditProfileScreen = ({navigation, route}) => {
 };
 
 export default EditAccountScreen;
-*/
