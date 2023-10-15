f9bc685e5d22fd8bdcab0d4dc31527eb29cc3845
clorange2 <140671227+clorange2@users.noreply.github.com>
2023-10-15 14:59:13 +1100

    Add files via upload

---
+++ React/Sub_AboutScreen/AboutApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AboutApp = ({navigation}) => {
+  return (
+    <View>
+      <Text>About</Text>
+    </View>
+  );
+};
+
+export default AboutApp;
