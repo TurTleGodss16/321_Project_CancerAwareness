87eee01a71f8f1d5af1e516faf1fe9c28fd71388
DJGankstar <117345527+DJGankstar@users.noreply.github.com>
2024-04-01 19:53:29 +1100

    Revert "Fix import case in App.js and add Firestore dependency in package.json"
    This reverts commit 0c8a4e17e78812415c8baa1161f5a7db6c8b58e9.

--- React/App.js
+++ React/App.js
@@ -26,7 +26,7 @@ import AboutPartners from './Sub_AboutScreen/AboutPartners';
 import AboutDevelopment from './Sub_AboutScreen/AboutDevelopment';
 import MultiLineHeaderTitle from './multiLineHeaderTitle';
 import LoginScreen from './Login';
-import SignupScreen from './Signup';
+import SignupScreen from './signup';
 
 const Stack = createNativeStackNavigator();
 
