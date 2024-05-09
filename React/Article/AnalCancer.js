40960dac7f4b1f0ad5a15da3bb49c6d3eef212f2
DJGankstar <117345527+DJGankstar@users.noreply.github.com>
2024-05-09 14:32:47 +1000

    chore: Refactor AnalCancer.js to remove unused imports and fix linting issues

--- React/Article/AnalCancer.js
+++ React/Article/AnalCancer.js
@@ -4,20 +4,13 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    TouchableOpacity,
-    ScrollView,
-    SafeAreaView
+    ScrollView
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { WebView } from 'react-native-webview';
 import { Dimensions } from 'react-native';
 
 const screenHeight = Dimensions.get('window').height;
-const Articles = ({ navigation }) => {
+const Articles = ({}) => {
     return(
         <ScrollView style={{ flex: 1 }}>
             <WebView nestedScrollEnabled
