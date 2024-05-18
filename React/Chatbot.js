0e28ceca73188a7b7893dc51f1a770cba073da13
DJGankstar <117345527+DJGankstar@users.noreply.github.com>
2024-05-19 00:20:22 +1000

    fixed chatbot - couldn't click start conversation

--- React/Chatbot.js
+++ React/Chatbot.js
@@ -1,29 +1,44 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable eol-last */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    SafeAreaView,
+} from 'react-native';
 import { WebView } from 'react-native-webview';
-import BottomNavigator from './BottomNavigator'; // Ensure this path is correct
+import BottomNavigator from './BottomNavigator';
+
+const screenHeight = Dimensions.get('window').height;
 
 const ChatbotScreen = ({ navigation }) => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.webViewContainer}>
-        <WebView
-          source={{ uri: 'https://creator.voiceflow.com/prototype/6639cc87dfee0b8ea6a1bfb5' }}
-          style={{ flex: 1 }}
-        />
-      </View>
-      <BottomNavigator />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.webViewContainer}>
+                <WebView 
+                    nestedScrollEnabled
+                    source={{ uri: 'https://creator.voiceflow.com/prototype/6639cc87dfee0b8ea6a1bfb5' }}
+                    style={styles.webView}
+                />
+            </View>
+            <BottomNavigator />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  webViewContainer: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
+    webViewContainer: {
+        flex: 1,
+    },
+    webView: {
+        flex: 1,
+    },
 });
 
 export default ChatbotScreen;
